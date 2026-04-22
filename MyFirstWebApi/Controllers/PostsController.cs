0e0ee6b51e0aabaac7da4ad09bb8aff6f24a6654
Julio Isidro <maravillas.isidro21@gmail.com>
2026-04-21 18:22:11 -0600

    feat: Crear mi primera web API

---
+++ MyFirstWebApi/Controllers/PostsController.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApi.Models;
+using MyFirstWebApi.Services;
+
+namespace MyApp.Namespace
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly PostService _postsService;
+        public PostsController()
+        {
+            _postsService = new PostService();
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Post>> GetPost(int id)
+        {
+            var post = await _postsService.GetPost(id);
+            if(post == null)
+                return NotFound();
+            return Ok(post);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Post>> CreatePost(Post post)
+        {
+            await _postsService.CreatePost(post);
+            return CreatedAtAction(nameof(GetPost), new {Id = post.Id}, post);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdatePost(int id, Post post)
+        {
+            if(id != post.Id)
+                return BadRequest();
+            
+            var updatedPost = await _postsService.UpdatePost(id,post);
+            if(updatedPost == null)
+                return NotFound();
+            
+            return Ok(post);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeletePost(int id)
+        {
+            await _postsService.DeletePost(id);
+            return Ok();
+        }
+
+        [HttpGet]
+        public async  Task<ActionResult<List<Post>>> GetPosts()
+        {
+            var posts = await _postsService.GetAllPosts();
+            return Ok(posts);
+        }
+
+    }
+}
